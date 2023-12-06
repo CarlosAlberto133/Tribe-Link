85ecfe5c547000aeb73eb7754fc45a289ac55b0d
Carlos Alberto <carlos.neurozze@gmail.com>
2023-12-06 17:07:46 -0300

    start project

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # tribe-link
 
+<p align="center">
+    <img width="600px" height="337px" src="src/assets/tribelink.gif" />
+</p>
+
 ## Project setup
 ```
 npm install
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = "https://ruxjfmcdcgkfdtyedkpm.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1eGpmbWNkY2drZmR0eWVka3BtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE3NzI3MDgsImV4cCI6MjAxNzM0ODcwOH0.uxrvFSMrE_KZPFyQ-BIOPHb4mfsm0IA6t1ruiJmUFCo"
+const supabaseKey = process.env.VUE_APP_API_KEY
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
